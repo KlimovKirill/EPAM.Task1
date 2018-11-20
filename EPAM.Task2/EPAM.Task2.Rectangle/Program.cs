e4f2803843b7c7456d2bd0b82516d1938ea63fc1
Kirill Klimov <kirillk22sbg@gmail.com>
2018-11-20 13:03:53 +0400

    t2

---
+++ EPAM.Task2/EPAM.Task2.Rectangle/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EPAM.Task2.Rectangle
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
