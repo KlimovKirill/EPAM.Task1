7ede7d11daf3f9de2ecf19bd4315733c6b6dd2a6
KlimovKirill <45049287+KlimovKirill@users.noreply.github.com>
2018-11-25 20:20:41 +0400

    almost 2nd task

---
+++ EPAM.Task2/EPAM.Task2.AverageStringLength/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPAM.Task2.AverageStringLength
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter string: ");
+            string inputString = Console.ReadLine();
+
+            int lengthOfCurrentWord = 0;
+            int sumOfLetters = 0;
+            int countOfWords = 0;
+
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                if (char.IsLetterOrDigit(inputString[i]))
+                {
+                    lengthOfCurrentWord++;
+                }
+
+                if (char.IsPunctuation(inputString[i]) || char.IsSeparator(inputString[i]) || i == inputString.Length - 1)
+                {
+                    sumOfLetters += lengthOfCurrentWord;
+                    lengthOfCurrentWord = 0;
+                    countOfWords++;
+                }
+            }
+
+            Console.WriteLine($"The average string length = {sumOfLetters/countOfWords}");
+
+        }
+    }
+}
--- EPAM.Task2/EPAM.Task2.Rectangle/Program.cs
+++ EPAM.Task2/EPAM.Task2.Rectangle/Program.cs
@@ -4,9 +4,25 @@ namespace EPAM.Task2.Rectangle
 {
     class MainClass
     {
+        public static int InputChecking()
+        {
+            int n;
+            n = int.Parse(Console.ReadLine());
+            
+
+            return n;
+        }
+
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            Console.WriteLine("Enter the side A of rectangle: ");
+            int a = InputChecking();
+
+            Console.WriteLine("Enter the side B of rectangle: ");
+            int b = InputChecking();
+
+            Console.WriteLine($"The area of the rectangle is equal to {a * b}");
         }
     }
 }
