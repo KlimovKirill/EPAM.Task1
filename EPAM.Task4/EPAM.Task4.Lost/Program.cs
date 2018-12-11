fed8ec7dac09eccad924338e45c5ce07e51e4428
Kirill Klimov <kirillk22sbg@gmail.com>
2018-12-11 10:01:28 +0400

    task4

---
+++ EPAM.Task4/EPAM.Task4.Lost/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EPAM.Task4.Lost
+{
+    class MainClass
+    {
+        public static int SearchingLastPerson(Queue<int> roundOfPeople)
+        {
+            bool flag = true;
+            while (roundOfPeople.Count != 1)
+            {
+                if (flag)
+                {
+                    roundOfPeople.Enqueue(roundOfPeople.Dequeue());
+                    flag = !flag;
+                }
+                else
+                {
+                    roundOfPeople.Dequeue();
+                    flag = !flag;
+                }
+            }
+
+            return roundOfPeople.Peek();
+        }
+
+        public static void Main(string[] args)
+        {
+            Queue<int> roundOfPeople = new Queue<int>();
+
+            Console.WriteLine("Enter amount of people N: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                roundOfPeople.Enqueue(i);
+            }
+
+            Console.WriteLine($"Last person is {SearchingLastPerson(roundOfPeople)}");
+        }
+    }
+}
